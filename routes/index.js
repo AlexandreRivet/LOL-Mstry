69c91ff0ca2358eba7ea312b7e93bc3d2c94777f
ThibaultCrosnier <thibault.crosnier@gmail.com>
2016-04-26 19:52:28 +0200

    Simple get summoner by name

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
+var express =   require('express'),
+    riot    =   require('lol-riot-api-module');
+
+var test = require("../data-test.json");
+
+var api = new riot({
+    key: test.API_KEY,
+    region: test.region
+});
 
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.render("index", {});
+});
+
+/*
+ *  Method: POST
+ *  URL: /data/summoner/name
+ *  Params:
+ *      ->  summonerName  | Name of the summoner
+ *      ->  region        | Region of the summoner
+ *  Desc.: Returns basic details about a summoner based on its name and its region
+ */
+router.post('/data/summoner/name', function(req, res, next) {
+
+    var params = {
+        "names": req.body.summonerName,
+        "region": req.body.region
+    };
+
+    api.getSummonersByNames(params).then(function(data){
+        var out = data[Object.keys(data)[0]];
+        console.log(out);
+        res.status(200).send(out);
+    });
 });
 
 module.exports = router;
