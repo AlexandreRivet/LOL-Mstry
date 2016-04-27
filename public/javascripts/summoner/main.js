05ce043b82c6767a31231b76b8d44ff99d95d310
alexandrerivet <alex-rivet94@hotmail.fr>
2016-04-27 18:10:36 +0200

    Update summoner view

--- public/javascripts/summoner/main.js
+++ public/javascripts/summoner/main.js
@@ -1,16 +1,5 @@
 $(document).init(function () {
 
-});
-
-
-$('#searchForSummoner').click(function () {
-
-	var region = $('.tabs .tab .active').html();
-	var summonerName = $('#summonerName_input').val();
-
-	if (summonerName == "" || region == "")
-		return;
-
-	loadSummoner(region, summonerName);
+	$('.button-collapse').sideNav();
 
 });
